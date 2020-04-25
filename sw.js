e374aa706afbcbdba1514390f2fe9827fc855fb7
ReghuNathan90 <24106366+ReghuNathan90@users.noreply.github.com>
2020-04-25 13:56:47 +0530

    files

---
+++ sw.js
@@ -0,0 +1,35 @@
+var cacheName = 'shopping';
+var filesToCache = [
+  './',
+  './index.html',
+  './css/style.css',
+  './css/bootstrap.css'
+  './css/font-awesome.css'
+  './css/skdslider.css',
+  './js/main.js',
+  './js/bootstrap.min.js',
+  './js/minicart.min.js',
+  './js/skdslider.min.js',
+  './js/skdslider.min.js'
+  './js/jquery-1.11.1.min.js'
+  '.js/move-top.js',
+  '.js/easing.js'
+];
+
+/* Start the service worker and cache all of the app's content */
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+/* Serve cached content when offline */
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
